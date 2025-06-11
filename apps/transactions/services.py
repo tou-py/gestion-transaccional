98a14c1991bb5541a2709890db1d47b6ed43ef8d
tou-py <sebastiantorresg2003@gmail.com>
2025-06-11 20:53:54 -0300

    update: implementa servicio base para asignacion, validacion y guardado, mejora minima en documentacion

--- apps/transactions/services.py
+++ apps/transactions/services.py
@@ -1,73 +1,77 @@
-from typing import Any, Optional, Dict
+from typing import Any, Optional, Dict, TypeVar
 
-from django.conf import settings
-from django.db import IntegrityError
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError, transaction
+from django.db.models import Model
 from rest_framework import serializers
 
 from .models import Account, Category, Transaction, Tag, Budget, ExchangeRate
 
+User = get_user_model()
 
-class AccountService:
+T = TypeVar("T", bound=Model)
+
+
+class BaseService:
     @staticmethod
-    def create_account(user: settings.AUTH_USER_MODEL, validated_data: Dict[str, Any]) -> Optional[Account]:
+    def _save_instance(instance: T, data: Dict[str, Any], uniqueness_error: Dict | str):
+        """
+        Asigna atributos, valida y guarda
+        uniqueness_error puede ser un diccionario para field-errors o str para non_field
+        """
+        for field, value in data.items():
+            setattr(instance, field, value)
+        try:
+            instance.full_clean()
+            instance.save()
+            return instance
+        except IntegrityError:
+            raise serializers.ValidationError(uniqueness_error)
+
+
+class AccountService(BaseService):
+    @staticmethod
+    def create_account(user: User, validated_data: Dict[str, Any]) -> Optional[Account]:
         try:
             return Account.objects.create(user=user, **validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una cuenta con ese nombre.'})
+            raise serializers.ValidationError({'non_field_errors': 'Ya existe una cuenta con ese nombre.'})
 
     @staticmethod
     def update_account(instance: Account, validated_data: Dict[str, Any]) -> Optional[Account]:
-        try:
-            for field, value in validated_data.items():
-                setattr(instance, field, value)
-            instance.full_clean()
-            instance.save()
-            return instance
-        except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una cuenta con ese nombre.'})
+        return BaseService._save_instance(instance, validated_data,
+                                          {'non_field_errors': "Ya existe una cuenta con ese nombre"})
 
 
-class CategoryService:
+class CategoryService(BaseService):
     @staticmethod
-    def create_category(user: settings.AUTH_USER_MODEL, validated_data: Dict[str, Any]) -> Optional[Category]:
+    def create_category(user: User, validated_data: Dict[str, Any]) -> Optional[Category]:
         try:
             return Category.objects.create(user=user, **validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una categoría con ese nombre y tipo.'})
+            raise serializers.ValidationError({'non_field_errors': 'Ya existe una categoría con ese nombre y tipo.'})
 
     @staticmethod
     def update_category(instance: Category, validated_data: Dict[str, Any]) -> Optional[Category]:
-        try:
-            for field, value in validated_data.items():
-                setattr(instance, field, value)
-            instance.full_clean()
-            instance.save()
-            return instance
-        except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una categoría con ese nombre y tipo.'})
+        return BaseService._save_instance(instance, validated_data,
+                                          {'non_field_errors': "Ya existe una categoria con ese nombre y tipo"})
 
 
-class TagService:
+class TagService(BaseService):
     @staticmethod
-    def create_tag(user: settings.AUTH_USER_MODEL, validated_data: Dict[str, Any]) -> Optional[Tag]:
+    def create_tag(user: User, validated_data: Dict[str, Any]) -> Optional[Tag]:
         try:
             return Tag.objects.create(user=user, **validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una etiqueta con ese nombre.'})
+            raise serializers.ValidationError({'non_field_errors': 'Ya existe una etiqueta con ese nombre.'})
 
     @staticmethod
     def update_tag(instance, validated_data):
-        try:
-            for field, value in validated_data.items():
-                setattr(instance, field, value)
-            instance.full_clean()
-            instance.save()
-            return instance
-        except IntegrityError:
-            raise serializers.ValidationError({'name': 'Ya existe una etiqueta con ese nombre.'})
+        return BaseService._save_instance(instance, validated_data,
+                                          {'non_field_errors': "Ya existe una etiqueta con ese nombre"})
 
 
-class ExchangeRateService:
+class ExchangeRateService(BaseService):
     @staticmethod
     def create_exchange_rate(validated_data: Dict[str, Any]) -> Optional[ExchangeRate]:
         try:
@@ -77,52 +81,37 @@ class ExchangeRateService:
 
     @staticmethod
     def update_exchange_rate(instance, validated_data):
-        try:
-            for field, value in validated_data.items():
-                setattr(instance, field, value)
-            instance.full_clean()
-            instance.save()
-            return instance
-        except IntegrityError:
-            raise serializers.ValidationError('Ya existe una tasa para estas monedas en esa fecha.')
+        return BaseService._save_instance(instance, validated_data,
+                                          {'non_field_errors': "Ya existe una tasa para estas monedas en esa fecha"})
 
 
-class TransactionService:
+class TransactionService(BaseService):
     @staticmethod
-    def create_transaction(user: settings.AUTH_USER_MODEL, validated_data: Dict[str, Any]) -> Optional[Transaction]:
-        # Extraer tags para manejarlos después de crear la transacción
+    def create_transaction(user: User, validated_data: Dict[str, Any]) -> Optional[Transaction]:
         tags_data = validated_data.pop('tags', [])
-
-        transaction = Transaction.objects.create(user=user, **validated_data)
-
-        # Asignar tags si existen
-        if tags_data:
-            for tag in tags_data:
-                transaction.tags.add(tag)
-
-        return transaction
+        with transaction.atomic():
+            tx = Transaction.objects.create(user=user, **validated_data)
+            if tags_data:
+                tx.tags.add(*tags_data)
+        return tx
 
     @staticmethod
-    def update_transaction(instance: Transaction, validated_data: Dict[str, Any]) -> Optional[Transaction]:
+    def update_transaction(instance, validated_data: Dict[str, Any]) -> Optional[Transaction]:
         tags_data = validated_data.pop('tags', None)
-
-        # Actualizar campos básicos
-        for field, value in validated_data.items():
-            setattr(instance, field, value)
-
-        instance.full_clean()
-        instance.save()
-
-        # Actualizar tags si se proporcionaron
-        if tags_data is not None:
-            instance.tags.set(tags_data)
-
-        return instance
+        with transaction.atomic():
+            # usa el helper genérico para campos simples
+            tx = BaseService._save_instance(
+                instance, validated_data,
+                'Error al actualizar la transacción.'
+            )
+            if tags_data is not None:
+                tx.tags.set(tags_data)
+        return tx
 
 
-class BudgetService:
+class BudgetService(BaseService):
     @staticmethod
-    def create_budget(user: settings.AUTH_USER_MODEL, validated_data: Dict[str, Any]) -> Optional[Budget]:
+    def create_budget(user: User, validated_data: Dict[str, Any]) -> Optional[Budget]:
         try:
             return Budget.objects.create(user=user, **validated_data)
         except IntegrityError:
@@ -130,11 +119,8 @@ class BudgetService:
 
     @staticmethod
     def update_budget(instance: Budget, validated_data: Dict[str, Any]) -> Optional[Budget]:
-        try:
-            for field, value in validated_data.items():
-                setattr(instance, field, value)
-            instance.full_clean()
-            instance.save()
-            return instance
-        except IntegrityError:
-            raise serializers.ValidationError('Ya existe un presupuesto para esta combinación.')
+        return BaseService._save_instance(
+            instance,
+            validated_data,
+            {"non_field_errors": "Ya existe un presupuesto con esa combinación"}
+        )
